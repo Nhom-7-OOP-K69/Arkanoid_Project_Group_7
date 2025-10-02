dfd38f6004efc50e2e0decc7dcfde6d382c153fd
mhaiisme <doanminhhaik6@gmail.com>
2025-10-03 00:10:01 +0700

    Test 1

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello Hải!");
             }
         });
 
@@ -28,5 +28,6 @@ public class Main extends Application {
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+
     }
 }
