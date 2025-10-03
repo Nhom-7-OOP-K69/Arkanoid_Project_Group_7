ce0775fa85462657fff4999f67ad2b9178975bb6
dzadanh <duyanh23112006@gmail.com>
2025-10-03 23:20:18 +0700

    danh 3

--- src/ExpandPaddlePowerUp.java
+++ src/ExpandPaddlePowerUp.java
@@ -1,2 +1,19 @@
-public class ExpandPaddlePowerUp extends PowerUp{
+public class ExpandPaddlePowerUp extends PowerUp {
+    private double expandSize = 40;
+
+    public ExpandPaddlePowerUp(double x, double y, double width, double height, int duration) {
+        super(x, y, width, height, 1, duration);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball) {
+        paddle.setWidth(paddle.getWidth() + expandSize);
+        paddle.setCurrentPowerUp(this.type);
+    }
+
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball){
+        paddle.setWidth(paddle.getWidth() - expandSize);
+        paddle.setCurrentPowerUp(0);
+    }
 }
--- src/FastBallPowerUp.java
+++ src/FastBallPowerUp.java
@@ -1,2 +1,21 @@
-public class FastBallPowerUp extends PowerUp{
+public class FastBallPowerUp extends PowerUp {
+    private double extraSpeed = 1.5;
+
+    public FastBallPowerUp(double x, double y, double width, double height, int duration){
+        super(x, y, width, height, 2, duration);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball){
+        ball.setDx(ball.getDx() * extraSpeed);
+        ball.setDy(ball.getDy() * extraSpeed);
+        paddle.setCurrentPowerUp(this.type);
+    }
+
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball){
+        ball.setDx(ball.getDx() / extraSpeed);
+        ball.setDy(ball.getDy() / extraSpeed);
+        paddle.setCurrentPowerUp(0);
+    }
 }
