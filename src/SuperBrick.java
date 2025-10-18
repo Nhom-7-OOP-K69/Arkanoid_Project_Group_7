3ce3f19a317e475bdf0679c10ca20199618ba1a6
tranduythuong <24021638@vnu.edu.vn>
2025-10-18 22:32:51 +0700

    thêm img, thêm constants, thêm hiệu ứng gạch

--- src/SuperBrick.java
+++ src/SuperBrick.java
@@ -1,17 +1,25 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.util.List;
+
 public class SuperBrick extends Brick {
-    private Image img;
+    private List<Image> img;
 
     public SuperBrick(double x, double y) {
         super(x, y, GameConstants.STRONG_BRICK_HP, GameConstants.STRONG_TYPE);
-        this.img = ImgManager.getInstance().getImage("PURPLE_BRICK");
+        img.add(ImgManager.getInstance().getImage("PURPLE_BRICK"));
+        img.add(ImgManager.getInstance().getImage("PURPLE_BRICK_"));
     }
 
     public void render(GraphicsContext gc) {
         if (!this.isDestroyed()) {
-            gc.drawImage(this.img, this.getX(), this.getY());
+            if (this.getHitPoints() >= GameConstants.STRONG_BRICK_HP) {
+                gc.drawImage(img.get(0), this.getX(), this.getY());
+            }
+            else  {
+                gc.drawImage(img.get(1), this.getX(), this.getY());
+            }
         }
     }
 }
