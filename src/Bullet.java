ac8b47d5818b2ee47347827f40c5c440c1ab56f5
dzadanh <duyanh23112006@gmail.com>
2025-10-21 19:51:50 +0700

    check

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,15 +1,42 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 public class Bullet extends MovableObject {
     private double bullet_speed = GameConstants.BULLET_SPEED;
+    private Image bulletImg; // ảnh đạn được lấy từ ImgManager
 
     public Bullet(double x, double y, double width, double height) {
         super(x, y, width, height);
+        // load ảnh đạn (LASER)
+        this.bulletImg = ImgManager.getInstance().getImage("LASER");
     }
+
+    public boolean intersects(Brick brick) {
+        return getX() < brick.getX() + brick.getWidth() &&
+                getX() + getWidth() > brick.getX() &&
+                getY() < brick.getY() + brick.getHeight() &&
+                getY() + getHeight() > brick.getY();
+    }
+
+
+
 
     public void update() {
         setY(getY() - bullet_speed);
     }
 
-    public void render() {
-        System.out.println("[Bullet] render tại (" + getX() + ", " + getY() + ")");
+    // Kiểm tra đạn đã ra khỏi màn hình chưa
+    public boolean isOutOfScreen() {
+        return getY() + getHeight() < 0;
+    }
+
+    public void render(GraphicsContext gc) {
+        if (bulletImg != null) {
+            gc.drawImage(bulletImg, getX(), getY(), getWidth(), getHeight());
+        } else {
+            // fallback nếu chưa load được ảnh
+            gc.setFill(javafx.scene.paint.Color.RED);
+            gc.fillRect(getX(), getY(), getWidth(), getHeight());
+        }
     }
 }
