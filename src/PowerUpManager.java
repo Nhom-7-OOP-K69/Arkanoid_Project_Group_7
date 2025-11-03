69f61928d9933fd4bc928861518da1e729ec5fd7
khangnb0111 <khangnb011106@gmail.com>
2025-11-03 15:15:58 +0700

    sửa lỗi

--- src/PowerUpManager.java
+++ src/PowerUpManager.java
@@ -17,7 +17,7 @@ public class PowerUpManager {
 
     // gọi khi brick bị phá
     public void spawnPowerUp(double x, double y) {
-        if (Math.random() < 0.9) { // 30% tỉ lệ rơi
+        if (Math.random() < 0.3) { // 30% tỉ lệ rơi
             double typeRand = Math.random();
             if (typeRand < 0.33) {
                 // Bullet
